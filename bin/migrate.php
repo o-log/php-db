494dc259579d1d258dc4d33bf21d6efda2fdcb68
Oleg Loginov <olognv@gmail.com>
2018-09-26 13:21:09 +0300

    migrate fix

--- bin/migrate.php
+++ bin/migrate.php
@@ -7,7 +7,7 @@ if (file_exists($project_autoload)) {
 } else {
     // when file in project_folder/vendor/o-log/bin
     $vendor_autoload = __DIR__ . '/../../../../vendor/autoload.php';
-    if (!file_exists($project_autoload)) {
+    if (!file_exists($vendor_autoload)) {
         throw new \Exception('autoload.php not found, probably needs composer install');
     }
     require_once $vendor_autoload;
