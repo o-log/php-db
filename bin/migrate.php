c29c05273fbffe0a67db4255332015e2ff91d95b
Oleg Loginov <olognv@gmail.com>
2018-09-26 13:07:15 +0300

    migrate -> migrate.php autoload detection improved

--- bin/migrate.php
+++ bin/migrate.php
@@ -1,4 +1,3 @@
-#!/usr/bin/php
 <?php
 
 // when file is in project_folder/bin
@@ -8,6 +7,9 @@ if (file_exists($project_autoload)) {
 } else {
     // when file in project_folder/vendor/o-log/bin
     $vendor_autoload = __DIR__ . '/../../../../vendor/autoload.php';
+    if (!file_exists($project_autoload)) {
+        throw new \Exception('autoload.php not found, probably needs composer install');
+    }
     require_once $vendor_autoload;
 }
 
